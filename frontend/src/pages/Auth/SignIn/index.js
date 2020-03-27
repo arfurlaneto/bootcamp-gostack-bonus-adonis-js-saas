fb8096ac6c3fdd3db04dcc45f4223ed386881ec4
arfurlaneto <arfurlaneto@gmail.com>
2020-03-26 21:07:51 -0300

    Login feature (layout and redux.)

--- frontend/src/pages/Auth/SignIn/index.js
+++ frontend/src/pages/Auth/SignIn/index.js
@@ -1,5 +1,74 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
-const SignIn = () => <div />;
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import AuthActions from '~/store/ducks/auth';
 
-export default SignIn;
+import Button from '~/styles/components/Button';
+import { Container, SignForm } from '../styles';
+
+class SignIn extends Component {
+  static propTypes = {
+    signInRequest: PropTypes.func.isRequired,
+  };
+
+  state = {
+    email: '',
+    password: '',
+  };
+
+  handleInputChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { email, password } = this.state;
+    const { signInRequest } = this.props;
+
+    signInRequest(email, password);
+  }
+
+  render() {
+    const { email, password } = this.state;
+
+    return (
+      <Container>
+        <SignForm onSubmit={this.handleSubmit}>
+          <h1>Boas vindas</h1>
+
+          <span>E-EMAIL</span>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={this.handleInputChange}
+          />
+
+          <span>SENHA</span>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={this.handleInputChange}
+          />
+
+          <Button size="big" type="submit">
+            Entrar
+          </Button>
+        </SignForm>
+      </Container>
+    );
+  }
+}
+
+const mapDispatchToProps = (dispatch) => bindActionCreators(AuthActions, dispatch);
+
+export default connect(
+  null,
+  mapDispatchToProps,
+)(SignIn);
